5da9bde8098bd539405a62288e7390c6bc3ed69b
Olivia Thorpe <othorpe@usf.edu>
2026-02-04 16:21:13 -0500

    Create Customer Records

---
+++ cc_3.js
@@ -0,0 +1,17 @@
+let Sarah = {
+    "name": "Sarah",
+    "email": "sarah@gmail.com",
+    "purchases": ["Milk", "Peanut Butter", "Yogurt"]
+};
+let Tom = {
+    "name": "Tom",
+    "email": "tom@gmail.com",
+    "purchases": ["Bread", "Butter", "Jam"]
+};
+let Emma = {
+    "name": "Emma",
+    "email": "emma@gmail.com",
+    "purchases": ["Eggs", "Bread", "Butter"]
+};
+
+let customers = [Sarah, Tom, Emma];
